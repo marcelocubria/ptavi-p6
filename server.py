f579eed56d72ff0e7ee31154236ea3e4b5a2e63f
Marcelo Cubria <marcelocubria@gmail.com>
2018-11-25 16:45:29 +0100

    añadida respuesta 400

--- server.py
+++ server.py
@@ -31,13 +31,20 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                     respuesta_invite += "SIP/2.0 180 Ringing\r\n\r\n"
                     respuesta_invite += "SIP/2.0 200 OK\r\n\r\n"
                     self.wfile.write(bytes(respuesta_invite, 'utf-8'))
+                else:
+                    self.wfile.write(b"SIP/2.0 400 Bad request\r\n\r\n")
             elif datos[0] == 'ACK':
                 aEjecutar = ("mp32rtp -i " + IP + " -p " + sys.argv[2] + " < "
                              + FICHERO_AUDIO)
                 print("ejecutando " + aEjecutar)
                 os.system(aEjecutar)
             elif datos[0] == 'BYE':
-                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                receptor = datos[1].split(':')[1].split('@')[0]
+                ip_ua = datos[1].split('@')[1]
+                if line[:-4] == (datos[0] + " sip:" + receptor + "@" + ip_ua + " SIP/2.0"):
+                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                else:
+                    self.wfile.write(b"SIP/2.0 400 Bad request\r\n\r\n")
             else:
                 self.wfile.write(b"SIP/2.0 405 Method Not Allowed")
 
