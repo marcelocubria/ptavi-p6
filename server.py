e32406b2195ce35d76e6cfe87887dc047b1b6651
Marcelo Cubria Alonso <mcubria@gamma29.aulas.gsyc.urjc.es>
2018-11-26 09:44:02 +0100

    funciona correctamente el programa

--- server.py
+++ server.py
@@ -24,6 +24,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 break
             datos = line.split(" ")
             if datos[0] == 'INVITE':
+                print("Llega " + line)
                 receptor = datos[1].split(':')[1].split('@')[0]
                 ip_ua = datos[1].split('@')[1]
                 if line[:-4] == (datos[0] + " sip:" + receptor + "@" + ip_ua +
@@ -35,11 +36,12 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 else:
                     self.wfile.write(b"SIP/2.0 400 Bad request\r\n\r\n")
             elif datos[0] == 'ACK':
-                aEjecutar = ("mp32rtp -i " + IP + " -p " + sys.argv[2] + " < "
-                             + FICHERO_AUDIO)
+                print("llega " + line)
+                aEjecutar = ('mp32rtp -i 127.0.0.1 -p 23032 < ' + FICH_AUDIO)
                 print("ejecutando " + aEjecutar)
                 os.system(aEjecutar)
             elif datos[0] == 'BYE':
+                print("llega " + line)
                 receptor = datos[1].split(':')[1].split('@')[0]
                 ip_ua = datos[1].split('@')[1]
                 if line[:-4] == (datos[0] + " sip:" + receptor + "@" + ip_ua +
@@ -55,7 +57,7 @@ if __name__ == "__main__":
     try:
         IP = sys.argv[1]
         PORT = int(sys.argv[2])
-        FICHERO_AUDIO = sys.argv[3]
+        FICH_AUDIO = sys.argv[3]
     except (IndexError, ValueError):
         sys.exit("Usage: python3 server.py IP port audio_file")
 
